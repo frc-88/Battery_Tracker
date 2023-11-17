2b24203fb82fecbeb2e493c8a0b03cdbe28014ba
Ned Ludwick <nedfrankie1228@gmail.com>
2023-11-16 22:23:51 -0500

    maybe numerical integral??

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-print("Hi ben warwick")
-print("this is a test")
+import numpy as np
+
+#get numerical integral from the user
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+n = int(input("Enter n: "))
+
+#calculate the width of each trapezoid
+h = (b-a)/n
+
+#calculate the x values
+x = np.linspace(a, b, n+1)
+
+#calculate the y values
+y = x**2
+
+#calculate the area under the curve using trapezoidal rule
+area = np.trapz(y, x)
+
+#output the result to console
+print("The area under the curve is: ", area)
