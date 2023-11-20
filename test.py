35e9f416b1b3fb2be15494556f875ee91561883c
Ned Ludwick <nedfrankie1228@gmail.com>
2023-11-20 15:35:15 -0500

    for loop

--- test.py
+++ test.py
@@ -5,17 +5,20 @@ a = float(input("Enter a: "))
 b = float(input("Enter b: "))
 n = int(input("Enter n: "))
 
-#calculate the width of each trapezoid
-h = (b-a)/n
+# calculate the width of each trapezoid
+h = (b - a) / n
 
-#calculate the x values
-x = np.linspace(a, b, n+1)
+# calculate the x values
+x = np.linspace(a, b, n + 1)
 
-#calculate the y values
-y = x**2
+# calculate the y values
+y = x ** 2
 
-#calculate the area under the curve using trapezoidal rule
-area = np.trapz(y, x)
+# calculate the area under the curve using trapezoidal rule
+area = 0
+for i in range(n):
+    area += (y[i] + y[i + 1]) * h / 2
 
-#output the result to console
-print("The area under the curve is: ", area)
+# output the result to console
+print("The area under the curve is:", area)
+ 
